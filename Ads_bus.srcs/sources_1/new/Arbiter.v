dd25829e335dda787c010742ae3a023d2b1d141a
Daksith Chandrasekera <daksith@gmail.com>
2023-03-12 20:37:05 +0530

    Inital commit

---
+++ Ads_bus.srcs/sources_1/new/Arbiter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/12/2023 06:45:04 PM
+// Design Name: 
+// Module Name: Arbiter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Arbiter(
+
+    );
+endmodule
---
+++ Ads_bus.srcs/sources_1/new/aribiter_params.vh
@@ -0,0 +1,13 @@
+//master id
+localparam master1 = 2'b00;
+localparam master2 = 2'b01;
+localparam master3 = 2'b10;
+
+//slave address
+localparam slave1 = 3'b000;
+localparam slave2 = 3'b001;
+localparam slave3 = 3'b010;
+
+//transfer types
+localparam read = 1'b0;
+localparam write = 1'b1;
